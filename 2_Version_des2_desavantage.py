752a1f598dbcc36f98da8e2acc82eba7aad0d68e
floflodev <6254fxppjh@privaterelay.appleid.com>
2026-03-24 19:08:18 +0100

    Update 2_Version_des2_desavantage.py

--- 2_Version_des2_desavantage.py
+++ 2_Version_des2_desavantage.py
@@ -1,16 +1,3 @@
-#import random 
-#j1 = input("Joueur 1 quel est ton prénom ?")
-#j2 = input("Joueur 2 quel est ton prénom ?")
+# Version 2 
+# Objectif : Créer un jeu de dés en désanvantageant un joueur en tronquant son dés pour qu'il ne puisse pas faire plus de 3
 
-#version 1
-#des1 = random.randint(1,6)
-#print(j1, "fait", des1)
-#des2 = random.randint(1,3)
-#print(j2, "fait", des2)
-
-#if des1 > des2 :
-#    print("Joueur 1 gagne")
-#elif des1 == des2 :
-#    print("Egalité")
-#else : 
-#    print("Joueur 2 gagne")
