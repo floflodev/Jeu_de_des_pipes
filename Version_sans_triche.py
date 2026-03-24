025ca2c5276648217532f1a6e576dd9848c522a3
floflodev <6254fxppjh@privaterelay.appleid.com>
2026-03-24 19:02:50 +0100

    Create a simple dice game for two players
    Implemented a simple dice game without cheating that allows two players to roll dice and determine the winner based on the results.

---
+++ Version_sans_triche.py
@@ -0,0 +1,26 @@
+# D'abord j'ai créee un jeu de dés simple sans tricherie 
+
+# Importe la bibliotheque random pour pouvoir avoir une selection aléatoire
+import random 
+# Demande le prénom des joueurs
+j1 = input("Joueur 1 quel est ton prénom ?")
+j2 = input("Joueur 2 quel est ton prénom ?")
+
+#Simule un réel lancé de dés en choissant un nombre aléatoire situés entre 1 et 6
+des1 = random.randint(1,6)
+# Affiche le resultat
+print(j1, "fait", des1)
+# Simule un nouveau lancé
+des2 = random.randint(1,6)
+print(j2, "fait", des2)
+
+# Compare les deux lancés et anonce le joueur 1 gagnant si le des1 est superieur a des2 
+if des1 > des2 :
+    print(j1, "gagne")
+# Compare les deux lancés et annonce une égalité si les deux des sont égaux
+elif des1 == des2 :
+    print("Egalité")
+# Annonce le joueur 2 gagne si les deux conditions précedentes ne sont pas respectées
+else : 
+    print(j2, "gagne")
+
