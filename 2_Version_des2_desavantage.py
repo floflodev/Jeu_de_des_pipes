8481f354ad407058aa4167b8516ad5780afd3bf9
floflodev <6254fxppjh@privaterelay.appleid.com>
2026-03-24 19:05:28 +0100

    Create 2_Version_des2_desavantage.py

---
+++ 2_Version_des2_desavantage.py
@@ -0,0 +1,16 @@
+#import random 
+#j1 = input("Joueur 1 quel est ton prénom ?")
+#j2 = input("Joueur 2 quel est ton prénom ?")
+
+#version 1
+#des1 = random.randint(1,6)
+#print(j1, "fait", des1)
+#des2 = random.randint(1,3)
+#print(j2, "fait", des2)
+
+#if des1 > des2 :
+#    print("Joueur 1 gagne")
+#elif des1 == des2 :
+#    print("Egalité")
+#else : 
+#    print("Joueur 2 gagne")
